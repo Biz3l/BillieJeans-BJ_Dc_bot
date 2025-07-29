483533e996525ccad4fc08a8a3e9fc971ebd9a77
Biz3l <gabrieloliveiralveslegal@gmail.com>
2025-07-29 15:21:30 -0300

    Atualizações básicas no código

--- bot.py
+++ bot.py
@@ -10,11 +10,12 @@ gemini_api = config("GOOGLEGEMINIAPI")
 
 genai.configure(api_key=gemini_api)
 model = genai.GenerativeModel("gemini-2.0-flash")
+prefix = "!"
 
 #Configurar Bot
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=f"{prefix}", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -41,7 +42,26 @@ async def mario(ctx):
     await ctx.send("https://pm1.aminoapps.com/6868/9bd680702e657d438cafd346a0304ded76b4ea3ar1-720-661v2_hq.jpg")
 
 @bot.command()
-async def miguel(ctx, palavra):
-    await ctx.send(f"Sim em minha concordância o Miguel é {palavra}")
+async def eleé(ctx, pessoa, palavra):
+    await ctx.send(f"Sim em minha concordância o(a) {pessoa} é {palavra}")
+    await ctx.send(f"Lembrando, ele gosta de tu hein cuidado")
+
+@bot.command()
+async def h(ctx):
+    await ctx.send(f"""
+Aqui estão alguns dos meus comandos!
+{prefix}ask
+Perguntas e respostas diretamente com inteligência artificial, caso dê algum erro,
+ provavelmente é pelo tamanho da sua pergunta/resposta!
+_______________________________________________________________________
+{prefix}ping
+Pong!
+_______________________________________________________________________
+{prefix}mario
+Um negócio meio nojento
+_______________________________________________________________________
+{prefix}eleé
+Envie um nome e uma palabvra que uma pessoa pode ser = Matheus Corno
+""")
 
 bot.run(dc_token)
